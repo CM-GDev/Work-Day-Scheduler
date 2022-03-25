e30df4233809588afd103721279fc074f3ab4975
Cristobal Marquez-Glynn <cristobalmqz@gmail.com>
2022-03-25 15:40:05 -0700

    Completed JS code for changing hour block color depending on time of day

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,19 +2,39 @@
 
 
 var workHours = 8;
-var startOfDay = moment("9:00","h:mm a")
+var startOfDay = moment("9:00am","h:mma")
 
-
-var today = moment().format("MMMM Do YYYY, h:mm:ss a");
+var today = moment().format("MMMM Do YYYY, h:mm:ssa");
 $("#currentDay").text(today);
+
+// var todayHour = moment().format("h:mm a")
+// console.log(todayHour)
 
 
 var hourBlocks = $(".container").children('form').children("label").children(".custom-hourBlock");
+// console.log(hourBlocks.length);
+
+var textAreas = $(".container").children('form').children("textarea");
+// console.log(textAreas.length);
+
+
 
 for (let i = 0; i < hourBlocks.length; i++) {
+      
+    var currentHour = startOfDay.clone().add(i, "hour");
+      
+    $(hourBlocks[i]).text(currentHour.format("h:mma"));
     
-    var currentHour = startOfDay.clone().add(i, "hour")
-    console.log(currentHour._d)
-    
-    $(hourBlocks[i]).text(currentHour.format("h:mm a"));
+       
+    if (moment(currentHour, "hh").isBefore(moment(),"hour")) {
+        
+        $(textAreas[i]).addClass("past");
+    }
+    if (moment(currentHour, "hh").isSame(moment(), "hour")) {
+        $(textAreas[i]).addClass("present");
+        console.log(currentHour.format("hh"));
+    } 
+    if (moment(currentHour, "hh").isAfter(moment(), "hour")) {
+        $(textAreas[i]).addClass("future");
+    }
 }
